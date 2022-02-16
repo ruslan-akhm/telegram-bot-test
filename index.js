e9ba4d1b0244761ec1736d4ec3f218c899d34dd9
Ruslan Akhmetshin <ruslan.akhmetshin@keydata.ca>
2022-02-16 15:27:02 -0500

    upd

--- index.js
+++ index.js
@@ -5,22 +5,18 @@ const token = "5183589885:AAH8K3-d4Fv4KQdfWz5g--XzPnxubKeuW6A";
 
 const bot = new TelegramApi(token, { polling: true });
 
-async function call() {
-	return await axios.get(
-		"https://evilinsult.com/generate_insult.php?lang=en&type=json"
-	);
-}
-
 bot.on("message", async (msg) => {
-	const text = msg.text;
 	const chatId = msg.chat.id;
 	console.log(msg);
-	if (text === "/command") {
-		return bot.sendPhoto(chatId, "./ilnur.jpg");
-	}
 	if (text === "/joke") {
-		let x = await call();
-		const joke = JSON.stringify(x.data.insult);
-		return bot.sendMessage(chatId, joke);
+		try {
+			let x = await axios.get(
+				"https://evilinsult.com/generate_insult.php?lang=en&type=json"
+			);
+			const joke = JSON.stringify(x.data.insult);
+			return await bot.sendMessage(chatId, joke);
+		} catch (err) {
+			return await bot.sendMessage(chatId, "err");
+		}
 	}
 });
