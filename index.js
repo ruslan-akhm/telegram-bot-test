1499103f57ef3e089018012beab4f44824d3c652
Ruslan Akhmetshin <ruslan.akhmetshin@keydata.ca>
2022-02-17 15:53:38 -0500

    upd bot

--- index.js
+++ index.js
@@ -8,15 +8,35 @@ const bot = new TelegramApi(token, { polling: true });
 bot.on("message", async (msg) => {
 	const chatId = msg.chat.id;
 	const text = msg.text;
-	if (text === "/joke") {
-		try {
-			let x = await axios.get(
-				"https://evilinsult.com/generate_insult.php?lang=en&type=json"
-			);
-			const joke = JSON.stringify(x.data.insult);
-			return await bot.sendMessage(chatId, joke);
-		} catch (err) {
-			return await bot.sendMessage(chatId, "err");
+	try {
+		let phrases = [
+			"Пашашундрик",
+			"Чирик-пиздык",
+			"Зачем вам это говно?",
+			"Пиздеть - не мешки ворочать",
+			"Ну как там с деньгами",
+			"Давай закругляйся",
+			"Здесь все, кто по кайфу",
+			"Заебал спамить",
+			"Хуево выглядишь",
+			"А где Рус?",
+			"Одобряю",
+			"Забаньте его в этом чате",
+			"Че может по пивку вечерком",
+			"Микрохайдаров",
+			"В чем сила",
+			"Это понятно, деващки?",
+			"Вы в курсе что я бот?",
+			"Нурикеша спросите",
+			"Айнурыч ёмаё",
+		];
+		const num = Math.floor(Math.random() * phrases.length);
+		let str = phrases[num];
+		const randomizer = Math.random();
+		if (randomizer < 0.1) {
+			return await bot.sendMessage(chatId, str);
 		}
+	} catch (err) {
+		return await bot.sendMessage(chatId, "err");
 	}
 });
