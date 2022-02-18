19268555cd5e202058d308cb245ac4fff5bab46c
Ruslan Akhmetshin <ruslan.akhmetshin@keydata.ca>
2022-02-18 09:59:23 -0500

    added webpack

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramApi = require("node-telegram-bot-api");
-const axios = require("axios");
+const phrases = require("./phrases");
 
 const token = process.env.MY_KEY;
 
@@ -9,30 +9,10 @@ bot.on("message", async (msg) => {
 	const chatId = msg.chat.id;
 	const text = msg.text;
 	try {
-		let phrases = [
-			"Пашашундрик",
-			"Чирик-пиздык",
-			"Зачем вам это говно?",
-			"Пиздеть - не мешки ворочать",
-			"Ну как там с деньгами",
-			"Давай закругляйся",
-			"Здесь все, кто по кайфу",
-			"Заебал спамить",
-			"Хуево выглядишь",
-			"А где Рус?",
-			"Одобряю",
-			"Забаньте его в этом чате",
-			"Че может по пивку вечерком",
-			"Микрохайдаров",
-			"В чем сила",
-			"Это понятно, деващки?",
-			"Вы в курсе что я бот?",
-			"Нурикеша спросите",
-			"Айнурыч ёмаё",
-		];
 		const num = Math.floor(Math.random() * phrases.length);
 		let str = phrases[num];
 		const randomizer = Math.random();
+		console.log(randomizer);
 		if (randomizer < 0.1) {
 			return await bot.sendMessage(chatId, str);
 		}
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+// This is main configuration object that tells Webpackw what to do.
+module.exports = {
+	//path to entry paint
+	entry: "./index.js",
+	//path and filename of the final output
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+
+	//default mode is production
+	mode: "development",
+};
